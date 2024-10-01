47616fc310a7db08e3cbf3f47c33f9743339c134
rubenfs <biorubenfs@hotmail.com>
2024-10-02 01:27:40 +0200

    ci: try ci

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,66 +1,15 @@
 import { Db, MongoClient } from 'mongodb'
 import config from './config.js'
-import { MongoMemoryReplSet } from 'mongodb-memory-server'
-
-// class Mongo {
-//   mongoClient!: MongoClient
-//   memoryReplSet?: MongoMemoryReplSet
-//   uri!: string
-
-//   async start (): Promise<void> {
-//     if (config.environment === 'test') {
-//       this.memoryReplSet = await MongoMemoryReplSet.create({ replSet: { count: 3 } })
-//       this.uri = this.memoryReplSet.getUri()
-
-//       // await while all SECONDARIES will be ready. Required only in testing.
-//       await new Promise((resolve) => setTimeout(resolve, 2500))
-//     } else {
-//       this.uri = config.mongo.uri
-//     }
-
-//     this.mongoClient = new MongoClient(this.uri, { retryWrites: true, w: 'majority' })
-
-//     await this.mongoClient.connect()
-
-//     console.log('connected mongodb')
-//   }
-
-//   get client (): MongoClient {
-//     return this.mongoClient
-//   }
-
-//   db (dbName?: string): Db {
-//     return this.mongoClient.db(dbName)
-//   }
-
-//   async stop (): Promise<void> {
-//     await this.mongoClient.close()
-//     if (this.memoryReplSet != null) {
-//       await this.memoryReplSet.stop()
-//     }
-//   }
-// }
-
-// export default new Mongo()
 
 class Mongo {
   mongoClient: MongoClient
-  memoryReplSet!: MongoMemoryReplSet
 
   constructor (mongoUri: string) {
     this.mongoClient = new MongoClient(mongoUri, { retryWrites: true, w: 'majority' })
   }
 
-  async start(): Promise<MongoClient> {
-    if (config.environment === "test") {
-      this.memoryReplSet = await MongoMemoryReplSet.create({ replSet: { count: 3 } })
-      this.mongoClient = await MongoClient.connect(this.memoryReplSet.getUri(), { retryWrites: true, w: 'majority' })
-
-      // await while all SECONDARIES will be ready. Required only in testing.
-      await new Promise((resolve) => setTimeout(resolve, 2500))
-    } else {
-      await this.mongoClient.connect()
-    }
+  async start (): Promise<MongoClient> {
+    await this.mongoClient.connect()
 
     console.log('connected mongodb')
 
@@ -76,9 +25,8 @@ class Mongo {
   }
 
   async stop (): Promise<void> {
-    await this.mongoClient.close()
-    if (this.memoryReplSet != null) {
-      await this.memoryReplSet.stop()
+    if (this.mongoClient != null) {
+      await this.mongoClient.close()
     }
   }
 }
