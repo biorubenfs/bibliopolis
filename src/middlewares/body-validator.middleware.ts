9755c179db915aece517e492d933f209c4910031
rubenfs <biorubenfs@hotmail.com>
2024-10-05 11:47:47 +0200

    style: fix eslint

--- src/middlewares/body-validator.middleware.ts
+++ src/middlewares/body-validator.middleware.ts
@@ -3,7 +3,6 @@ import { NextFunction, Request, Response } from 'express'
 import { ZodSchema } from 'zod'
 import { InvalidBodyError } from '../error/errors.js'
 
-
 function bodyValidator (schema: ZodSchema): (req: Request, res: Response, next: NextFunction) => void {
   return function (req: Request, res: Response, next: NextFunction): void {
     const validationResult = schema.safeParse(req.body)
