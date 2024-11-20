4609711c6e0af5a6cb4780a7c01e016c46caca77
rubenfs <biorubenfs@hotmail.com>
2024-11-20 10:34:25 +0100

    fix: remove wrong arg in pagination validation middleware function

--- src/middlewares/pagination-validator.middleware.ts
+++ src/middlewares/pagination-validator.middleware.ts
@@ -8,10 +8,10 @@ const DEFAULT_LIMIT_STRING_VALUE = '10'
 export function queryPaginationValidator (req: Request, res: Response, next: NextFunction): void {
   const paginationSchema: Zod.Schema = z.object({
     skip: z.string().optional()
-      .transform((val) => parseInt(val ?? DEFAULT_SKIP_STRING_VALUE, 0))
-      .refine(val => !isNaN(val) && val >= 0, 'skip must be a number greater than o equal >= 0'),
+      .transform((val) => parseInt(val ?? DEFAULT_SKIP_STRING_VALUE))
+      .refine(val => !isNaN(val) && val >= 0, 'skip must be a number >= 0'),
     limit: z.string().optional()
-      .transform((val) => parseInt(val ?? DEFAULT_LIMIT_STRING_VALUE, 10))
+      .transform((val) => parseInt(val ?? DEFAULT_LIMIT_STRING_VALUE))
       .refine(val => !isNaN(val) && val > 0 && val <= 100, 'limit must be a number > 0 and <= 100')
   })
 
