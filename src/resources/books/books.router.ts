ce829aa08d886007cfbed3a64c6871075b29e5a9
rubenfs <biorubenfs@hotmail.com>
2024-12-13 23:47:38 +0100

    wip: add status code in router responses

--- src/resources/books/books.router.ts
+++ src/resources/books/books.router.ts
@@ -5,11 +5,27 @@ import { newBookSchema } from './books.schema.js'
 import booksService from './books.service.js'
 import { queryPaginationValidator } from '../../middlewares/pagination-validator.middleware.js'
 import { parseSkipLimitQP } from '../../utils.js'
+import { HttpStatusCode } from '../../types.js'
+import { ResultObject } from '../../results.js'
 
 const booksRouter = Router()
 
-booksRouter.post('/', bodyValidator(newBookSchema), tryCatch(async (req) => await booksService.create(req.body)))
-booksRouter.get('/:id', tryCatch(async (req) => await booksService.getById(req.params.id)))
-booksRouter.get('/', queryPaginationValidator, tryCatch(async (req) => await booksService.list(parseSkipLimitQP(req))))
+booksRouter.post('/', bodyValidator(newBookSchema), tryCatch(async (req) => {
+  const result = await booksService.create(req.body)
+
+  return ResultObject.toFinal(HttpStatusCode.Created, result)
+}))
+
+booksRouter.get('/:id', tryCatch(async (req) => {
+  const result = await booksService.getById(req.params.id)
+
+  return ResultObject.toFinal(HttpStatusCode.OK, result)
+}))
+
+booksRouter.get('/', queryPaginationValidator, tryCatch(async (req) => {
+  const results = await booksService.list(parseSkipLimitQP(req))
+
+  return ResultObject.toFinal(HttpStatusCode.OK, results)
+}))
 
 export default booksRouter
--- src/resources/libraries/libraries.router.ts
+++ src/resources/libraries/libraries.router.ts
@@ -9,17 +9,39 @@ import { Role } from '../users/users.interfaces.js'
 import librariesBooksService from '../libraries-books/libraries-books.service.js'
 import { queryPaginationValidator } from '../../middlewares/pagination-validator.middleware.js'
 import { parseSkipLimitQP } from '../../utils.js'
+import { ResultObject } from '../../results.js'
+import { HttpStatusCode } from '../../types.js'
 
 const librariesRouter = Router()
 
-librariesRouter.post('/', bodyValidator(newLibrarySchema), checkJwt, tryCatch(async (req) => await librariesService.create(req.body, req.userId ?? '')))
-librariesRouter.get('/:id', tryCatch(async (req) => await librariesService.get(req.params.id, req.userId ?? '', req.role ?? Role.Regular)))
-librariesRouter.get('/', queryPaginationValidator, tryCatch(async (req) => await librariesService.list(req.userId ?? '', req.role ?? Role.Regular, parseSkipLimitQP(req))))
+librariesRouter.post('/', bodyValidator(newLibrarySchema), checkJwt, tryCatch(async (req) => {
+  const result = await librariesService.create(req.body, req.userId ?? '')
+  return ResultObject.toFinal(HttpStatusCode.Created, result)
+}))
 
-librariesRouter.post('/:id/books', bodyValidator(bookdIdSchema), tryCatch(async (req) => await librariesService.addBook(req.params.id, req.body.id, req.userId ?? '')))
-librariesRouter.delete('/:id/books', bodyValidator(bookdIdSchema), tryCatch(async (req) => await librariesService.removeBook(req.params.id, req.body.id, req.userId ?? '')))
+librariesRouter.get('/:id', tryCatch(async (req) => {
+  const result = await librariesService.get(req.params.id, req.userId ?? '', req.role ?? Role.Regular)
+  return ResultObject.toFinal(HttpStatusCode.OK, result)
+}))
+
+librariesRouter.get('/', queryPaginationValidator, tryCatch(async (req) => {
+  const result = await librariesService.list(req.userId ?? '', req.role ?? Role.Regular, parseSkipLimitQP(req))
+  return ResultObject.toFinal(HttpStatusCode.OK, result)
+}))
+
+librariesRouter.post('/:id/books', bodyValidator(bookdIdSchema), tryCatch(async (req) => {
+  const result = await librariesService.addBook(req.params.id, req.body.id, req.userId ?? '')
+  return ResultObject.toFinal(HttpStatusCode.Created, result)
+}))
+librariesRouter.delete('/:id/books', bodyValidator(bookdIdSchema), tryCatch(async (req) => {
+  const result = await librariesService.removeBook(req.params.id, req.body.id, req.userId ?? '')
+  return ResultObject.toFinal(HttpStatusCode.NoContent, result)
+}))
 
 // Libraries-books
-librariesRouter.get('/:id/books', queryPaginationValidator, tryCatch(async (req) => await librariesBooksService.list(req.params.id, req.userId ?? '', parseSkipLimitQP(req))))
+librariesRouter.get('/:id/books', queryPaginationValidator, tryCatch(async (req) => {
+  const result = await librariesBooksService.list(req.params.id, req.userId ?? '', parseSkipLimitQP(req))
+  return ResultObject.toFinal(HttpStatusCode.OK, result)
+}))
 
 export default librariesRouter
