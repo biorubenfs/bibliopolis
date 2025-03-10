9eb585ae41f79c8b3381e809a3ca4c894a226c0e
rubenfs <biorubenfs@hotmail.com>
2025-03-10 01:21:27 +0100

    chore: add drop database in seed database script

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -4,6 +4,7 @@ import mongo from '../mongo.js'
 async function seedDatabase (): Promise<void> {
   await mongo.start()
 
+  await mongo.db().dropDatabase()
   await loadDataInDb(DataSetType.Seed, MockDataSet.Books, MockDataSet.Libraries, MockDataSet.LibrariesBooks, MockDataSet.Users)
 
   await mongo.stop()
