e61c417324b12b32ce4ed862596cd3e6af368c5b
Ruben Fernandez <65553496+biorubenfs@users.noreply.github.com>
2025-04-14 00:13:47 +0200

    chore: add open library class to manage new books (#4)

--- src/resources/libraries/open-library/api.ts
+++ src/resources/libraries/open-library/api.ts
@@ -1,5 +1,5 @@
-import config from '../../../config'
-import { Author, Edition } from './types'
+import config from '../../../config.js'
+import { Author, OpenLibraryBook } from './types.js'
 
 class OpenLibraryApi {
   private readonly domain: URL
@@ -8,15 +8,15 @@ class OpenLibraryApi {
     this.domain = domain
   }
 
-  async fetchBookByEdition (editionId: string): Promise<Edition> {
+  async fetchBookByEdition (editionId: string): Promise<OpenLibraryBook> {
     const url = new URL(`/books/${editionId}.json`, this.domain)
     const response = await fetch(url)
-    const data: Edition = await response.json()
+    const data: OpenLibraryBook = await response.json()
     return data
   }
 
   async fetchAuthorById (identifierKey: string): Promise<Author> {
-    const url = new URL(identifierKey, this.domain)
+    const url = new URL(`${identifierKey}.json`, this.domain)
     const response = await fetch(url)
     const data: Author = await response.json()
     return data
