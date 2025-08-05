3e67423d87b38e2fed839d0b5260c128d88113a8
biorubenfs <biorubenfs@hotmail.com>
2025-08-06 00:06:13 +0200

    chore: add defaults in config to pass tests

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,22 @@ import { z } from 'zod'
 const envSchema = z.object({
   ENVIRONMENT: z.string().default('dev'),
   PORT: z.string().default('3000').transform(value => parseInt(value)),
-  MONGO_URI: z.string(),
-  DEFAULT_ADMIN_NAME: z.string(),
-  DEFAULT_ADMIN_EMAIL: z.string().email(),
-  DEFAULT_ADMIN_PASSWORD: z.string(),
-  JWT_SECRET: z.string(),
+  MONGO_URI: z.string().default('mongodb://localhost:27017/bibliopolis'),
+  // Default admin user
+  DEFAULT_ADMIN_NAME: z.string().default('admin01'),
+  DEFAULT_ADMIN_EMAIL: z.string().email().default('admin01@email.com'),
+  DEFAULT_ADMIN_PASSWORD: z.string().default('Password123$'),
+  // JWT configuration
+  JWT_SECRET: z.string().default('supersecretkey'),
+  EXPIRATION_TIME: z.string().optional().default('60000'),
   HASH_ROUNDS: z.string().optional().default('10').transform(value => parseInt(value)),
-  OL_COVER_URL_PATH: z.string()
+  // Open Library config
+  OPEN_LIBRARY_DOMAIN: z.string().optional().default('https://openlibrary.org').transform(value => new URL(value)),
+  OL_COVER_URL_PATH: z.string().default('none'),
+  // Cookie options
+  COOKIE_OPTIONS_HTTP_ONLY: z.boolean().optional().default(true).transform(value => value === true),
+  COOKIE_OPTIONS_SECURE: z.boolean().optional().default(false).transform(value => value === true),
+  COOKIE_OPTIONS_SAME_SITE: z.string().optional().default('none'),
 })
 
 const { data, error, success } = envSchema.safeParse(process.env)
@@ -17,10 +26,6 @@ const { data, error, success } = envSchema.safeParse(process.env)
 if (!success) {
   console.log(error.issues)
   throw new Error('invalid environment config')
-}
-
-function parseUrl (value?: string, dflt = ''): URL {
-  return new URL(value ?? dflt)
 }
 
 export default {
@@ -35,10 +40,17 @@ export default {
     password: data.DEFAULT_ADMIN_PASSWORD
   },
   jwt: {
+    expirationTime: data.EXPIRATION_TIME,
     secret: data.JWT_SECRET
   },
   hashRounds: data.HASH_ROUNDS,
   openLibrary: {
+    domain: data.OPEN_LIBRARY_DOMAIN,
     coverUrlPattern: data.OL_COVER_URL_PATH
+  },
+  cookieOptions: {
+    httpOnly: data.COOKIE_OPTIONS_HTTP_ONLY,
+    secure: data.COOKIE_OPTIONS_SECURE,
+    sameSite:  data.COOKIE_OPTIONS_SAME_SITE
   }
 }
