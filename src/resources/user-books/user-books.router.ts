4e9b8d16de87981771010c3eddc1dd7d68230026
rubenfs <biorubenfs@hotmail.com>
2026-02-23 00:26:50 +0100

    refactor: refactor params in user books router

--- src/resources/user-books/user-books.router.ts
+++ src/resources/user-books/user-books.router.ts
@@ -11,18 +11,18 @@ import { Role } from '../users/users.interfaces.js'
 const userBooksRouter = Router()
 
 userBooksRouter.get('/', queryPaginationValidator, tryCatch(async (req) => {
-  const librariesIds = ensureArray(req.query.libraries as string | string[] | null)
+  const librariesIds = ensureArray(req.query.libraryId as string | string[] | null)
   const result = await userBooksService.list(parseSkipLimitQP(req), req.userId ?? '', req.role ?? Role.Regular, { userId: req.query.userId as string, librariesIds: librariesIds ?? undefined })
   return { status: HttpStatusCode.OK, data: result }
 }))
 
-userBooksRouter.get('/:bookId', tryCatch(async (req) => {
-  const result = await userBooksService.get(req.params.bookId, req.userId ?? '')
+userBooksRouter.get('/:id', tryCatch(async (req) => {
+  const result = await userBooksService.get(req.params.id, req.userId ?? '')
   return { status: HttpStatusCode.OK, data: result }
 }))
 
-userBooksRouter.patch('/:bookId', bodyValidator(userBookUpdateSchema), tryCatch(async (req) => {
-  const result = await userBooksService.update(req.params.bookId, req.userId ?? '', req.body)
+userBooksRouter.patch('/:id', bodyValidator(userBookUpdateSchema), tryCatch(async (req) => {
+  const result = await userBooksService.update(req.params.id, req.userId ?? '', req.body)
   return { status: HttpStatusCode.OK, data: result }
 }))
 
