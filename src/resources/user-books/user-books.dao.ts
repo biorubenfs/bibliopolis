5f860edab108947821454b4cc23279108aeadce3
rubenfs <biorubenfs@hotmail.com>
2026-03-03 23:21:04 +0100

    style: fix eslint

--- src/resources/user-books/user-books.dao.ts
+++ src/resources/user-books/user-books.dao.ts
@@ -3,7 +3,7 @@ import Dao from '../../dao.js'
 import { isNotNull } from '../../utils.js'
 import { DBUserBook, UpdateUserBook } from './user-books.interfaces.js'
 import { UserBookEntity } from './user-books.entity.js'
-import { AggregationCursor, Document, WithId, ClientSession  } from 'mongodb'
+import { AggregationCursor, Document, WithId, ClientSession } from 'mongodb'
 import { BookEntity } from '../books/books.entity.js'
 import config from '../../config.js'
 
