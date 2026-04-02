e8540006042f0c16f65f83e66453f29a1188b582
rubenfs <biorubenfs@hotmail.com>
2026-04-02 02:13:38 +0200

    fix: fix config in cookie path

--- src/config.ts
+++ src/config.ts
@@ -40,7 +40,7 @@ export default {
       httpOnly: true,
       secure: parseBoolean(process.env.REFRESH_TOKEN_COOKIE_SECURE, false),
       sameSite: parseString(process.env.REFRESH_TOKEN_COOKIE_SAME_SITE, 'lax') as 'strict' | 'lax' | 'none',
-      path: '/auth'
+      path: process.env.ENVIRONMENT === 'dev' ? '/api/auth' : '/auth' // TODO: FIX ME
     }
   },
   hashRounds: parseNumber(process.env.HASH_ROUNDS, 10),
