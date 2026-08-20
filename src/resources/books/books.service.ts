3e7988e0576c6a476dcc32421ba849be9d0b7283
rubenfs <biorubenfs@hotmail.com>
2026-08-20 10:40:08 +0200

    refactor: refactor create method in book service to use indexes

--- src/resources/books/books.service.ts
+++ src/resources/books/books.service.ts
@@ -1,3 +1,4 @@
+import { MongoServerError } from 'mongodb'
 import { CollectionResultObject, SingleResultObject } from '../../results.js'
 import { Page } from '../../types.js'
 import { ISBNUtils } from '../../utils/isbn.utils.js'
@@ -10,30 +11,21 @@ class BooksService {
   async create (body: NewBook): Promise<SingleResultObject<BookEntity>> {
     const { isbn10, isbn13 } = ISBNUtils.calculateIsbns(body.isbn10, body.isbn13)
 
-    // Check for duplicates by ISBN-13
-    const existingByIsbn13 = await booksDao.findByIsbn13(isbn13)
-    if (existingByIsbn13 != null) {
-      throw new BookAlreadyExistsError(`there is already a book with isbn13 ${isbn13}`)
-    }
-
-    // Check for duplicates by ISBN-10 (if it exists)
-    if (isbn10 != null) {
-      const existingByIsbn10 = await booksDao.findByIsbn10(isbn10)
-      if (existingByIsbn10 != null) {
-        throw new BookAlreadyExistsError(`there is already a book with isbn10 ${isbn10}`)
+    try {
+      const newBook = await booksDao.create({
+        title: body.title,
+        authors: body.authors,
+        isbn13,
+        isbn10,
+        cover: body.cover ?? null
+      })
+      return new SingleResultObject(newBook)
+    } catch (error) {
+      if (error instanceof MongoServerError && error.code === 11000) {
+        throw new BookAlreadyExistsError(`there is already a book with the same ISBN`)
       }
+      throw error
     }
-
-    const bookData = {
-      title: body.title,
-      authors: body.authors,
-      isbn13,
-      isbn10,
-      cover: body.cover ?? null
-    }
-
-    const newBook = await booksDao.create(bookData)
-    return new SingleResultObject(newBook)
   }
 
   async fetchById (id: string): Promise<BookEntity | null> {
