c3cbe9adb8c8e6777ff20ae142c0e17dc4500b7b
rubenfs <biorubenfs@hotmail.com>
2026-08-25 16:51:50 +0200

    chore: add skip property based on environment in rate limiters

--- src/rate-limiters.ts
+++ src/rate-limiters.ts
@@ -1,5 +1,7 @@
 import rateLimit from 'express-rate-limit'
 
+const isTestEnv = process.env.NODE_ENV === 'test'
+
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 10,
@@ -8,14 +10,16 @@ const authLimiter = rateLimit({
   message: {
     success: false,
     message: 'Too many requests, please try again later.'
-  }
+  },
+  skip: () => isTestEnv
 })
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 1000,
   standardHeaders: true,
-  legacyHeaders: false
+  legacyHeaders: false,
+  skip: () => isTestEnv
 })
 
 export { authLimiter, apiLimiter }
